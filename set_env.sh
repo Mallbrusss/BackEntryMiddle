5e65acce3e25738c537ebe5209958faef35ac6d7
e.bogachev <e.bogachev@digitaleconomy.space>
2024-11-06 01:33:01 +0300

    first work version

--- set_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-IP_POSTGRES=&(getent hosts postgres | awk '{print $1}')
-IP_REDIS=$(getent hosts redis | awk '{ print $1 }')
-
-echo "POSTGRES_HOST=${IP_POSTGRES}" > .env
-echo "REDIS_HOST=${IP_REDIS}" >> .env
-
-exec ./app-mid
